2dd2ab1e0c735bda2e92c1d8b5a272d1ec417f07
Logan King <seanloganking@gmail.com>
2022-06-12 18:11:40 -0400

    screen_pos_to_coord()
    fixed #2

--- src/main.rs
+++ src/main.rs
@@ -3,19 +3,19 @@ use std::{collections::HashMap, path::PathBuf};
 
 use macroquad::prelude::*;
 
-#[derive(Debug)]
-struct Bounds {
-    max_x: i32,
-    max_z: i32,
-    min_x: i32,
-    min_z: i32,
-}
+// #[derive(Debug)]
+// struct Bounds {
+//     max_x: i32,
+//     max_z: i32,
+//     min_x: i32,
+//     min_z: i32,
+// }
 
-struct FilenameAndNumbers {
-    file_name: PathBuf,
-    x: i32,
-    z: i32,
-}
+// struct FilenameAndNumbers {
+//     file_name: PathBuf,
+//     x: i32,
+//     z: i32,
+// }
 
 /// Returns a list of all files in a directory and it's subdirectories
 fn get_files_in_dir(path: &str, filetype: &str) -> Result<Vec<PathBuf>, GlobError> {
@@ -46,9 +46,13 @@ fn get_files_in_dir(path: &str, filetype: &str) -> Result<Vec<PathBuf>, GlobErro
     Ok(paths)
 }
 
-const tile_dir: &str = "./terrain/";
+const TILE_DIR: &str = "./terrain/";
 
-async fn get_textures_for_zoom_level(level: u32, directory: &str, tile_dimensions: (f32,f32)) -> HashMap<(i32, i32), Texture2D> {
+async fn get_textures_for_zoom_level(
+    level: u32,
+    directory: &str,
+    tile_dimensions: (f32, f32),
+) -> HashMap<(i32, i32), Texture2D> {
     let files = get_files_in_dir(&(directory.to_owned() + &level.to_string()), "").unwrap();
 
     let mut sector_to_texture = HashMap::new();
@@ -63,8 +67,8 @@ async fn get_textures_for_zoom_level(level: u32, directory: &str, tile_dimension
         // map sector to texture
         let texture: Texture2D = load_texture(file.to_str().unwrap()).await.unwrap();
         texture.set_filter(FilterMode::Nearest);
-        if texture.width() != tile_dimensions.0 || texture.height() != tile_dimensions.1{
-            panic!("File: \"{}\" has differing dimensions",file_name)
+        if texture.width() != tile_dimensions.0 || texture.height() != tile_dimensions.1 {
+            panic!("File: \"{}\" has differing dimensions", file_name)
         }
         sector_to_texture.insert((x, z), texture);
     }
@@ -74,32 +78,13 @@ async fn get_textures_for_zoom_level(level: u32, directory: &str, tile_dimension
 
 fn coord_to_screen_pos(x: i32, y: i32, camera: &CameraSettings) -> (f32, f32) {
     let out_x = screen_width() / 2. + ((camera.x_offset + x as f32) * camera.zoom_multiplier);
-
     let out_y = screen_height() / 2. + ((camera.y_offset + y as f32) * camera.zoom_multiplier);
-
     (out_x, out_y)
 }
 
-// fn screen_pos_to_coord(x: f32, y: f32, camera: &CameraSettings) -> (f32, f32) {
-
-//     let out_x = camera.x_offsetx as f32 / 2.;
-
-//     todo!()
-// }
-
 fn screen_pos_to_coord(x: f32, y: f32, camera: &CameraSettings) -> (f32, f32) {
-    let x_out = (-camera.x_offset - screen_width() / 4.) / camera.zoom_multiplier
-        + (x as f32 / 2. / camera.zoom_multiplier);
-    let y_out = (-camera.y_offset - screen_height() / 4.) / camera.zoom_multiplier
-        + (y as f32 / 2. / camera.zoom_multiplier);
-
-    // let x_out = (camera.x_offset / camera.zoom_multiplier);
-    // let y_out = (camera.y_offset / camera.zoom_multiplier);
-
-    // camera.zoom_multiplier * tile_dimensions.0
-
-    // tile width matters
-
+    let x_out = -camera.x_offset + (x as f32 - screen_width() / 2.) / camera.zoom_multiplier;
+    let y_out = -camera.y_offset + (y as f32 - screen_height() / 2.) / camera.zoom_multiplier;
     (x_out, y_out)
 }
 
@@ -115,11 +100,11 @@ fn value_in_range(value: f32, min: f32, max: f32) -> bool {
 }
 
 /// returns true if two rectangles overlap
-/// 
+///
 /// Resources:
-/// 
+///
 /// https://stackoverflow.com/questions/306316/determine-if-two-rectangles-overlap-each-other
-/// 
+///
 /// https://silentmatt.com/rectangle-intersection/
 fn rectangle_overlap(a: Rectangle, b: Rectangle) -> bool {
     let x_overlap =
@@ -145,23 +130,25 @@ async fn main() {
         zoom_multiplier: 1.0,
     };
 
-    let mut tile_dimensions: (f32,f32) = (0.,0.);
+    let mut tile_dimensions: (f32, f32) = (0., 0.);
 
     // load texture cache
     let mut max_zoom_level: u32 = 0;
     let mut texture_cache: Vec<HashMap<(i32, i32), Texture2D>> = Vec::new();
     for x in 0.. {
-        if PathBuf::from(tile_dir.to_owned() + &x.to_string()).is_dir() {
+        if PathBuf::from(TILE_DIR.to_owned() + &x.to_string()).is_dir() {
             // get initial tile dimensions
-            if x == 0{
-                let files = get_files_in_dir(&(tile_dir.to_owned() + &0.to_string()), "").unwrap();
+            if x == 0 {
+                let files = get_files_in_dir(&(TILE_DIR.to_owned() + &0.to_string()), "").unwrap();
 
                 let texture: Texture2D = load_texture(files[0].to_str().unwrap()).await.unwrap();
                 tile_dimensions.0 = texture.width();
                 tile_dimensions.1 = texture.height();
             }
 
-            texture_cache.push(get_textures_for_zoom_level(x.try_into().unwrap(), tile_dir, tile_dimensions).await);
+            texture_cache.push(
+                get_textures_for_zoom_level(x.try_into().unwrap(), TILE_DIR, tile_dimensions).await,
+            );
             max_zoom_level = x;
         } else {
             break;
@@ -350,35 +337,29 @@ async fn main() {
             WHITE,
         );
 
-        // draw_text(
-        //     &("camera.x_offset: ".to_owned() + &camera.x_offset.to_string()),
-        //     20.0,
-        //     20.0,
-        //     30.0,
-        //     WHITE,
-        // );
+        let mouse = mouse_position();
+        draw_circle(mouse.0, mouse.1, 5.0, BLUE);
+        let mouse_coord = screen_pos_to_coord(mouse.0, mouse.1, &camera);
+        draw_text(
+            &("mouse.x: ".to_owned() + &mouse_coord.0.to_string()),
+            20.0,
+            100.0,
+            30.0,
+            WHITE,
+        );
 
-        //red circle
-        // let red_circle_x = screen_width() / 2.;
-        // let red_circle_y = screen_height() / 2.;
-        // let screen_pos = screen_pos_to_coord(red_circle_x, red_circle_y, &camera);
-        // draw_circle(red_circle_x, red_circle_y, 15.0, RED);
-
-        // let mouse = mouse_position();
-        // draw_circle(mouse.0, mouse.1, 15.0, BLUE);
-        // let mouse_coord = screen_pos_to_coord(mouse.0, mouse.1, &camera);
-        // draw_text(
-        //     &("mouse.x: ".to_owned() + &mouse_coord.0.to_string()),
-        //     20.0,
-        //     60.0,
-        //     30.0,
-        //     WHITE,
-        // );
+        draw_text(
+            &("mouse.y: ".to_owned() + &mouse_coord.1.to_string()),
+            20.0,
+            120.0,
+            30.0,
+            WHITE,
+        );
 
         // draw_text(
-        //     &("mouse.y: ".to_owned() + &mouse_coord.1.to_string()),
+        //     &("camera.x_offset: ".to_owned() + &(camera.x_offset).to_string()),
         //     20.0,
-        //     80.0,
+        //     140.0,
         //     30.0,
         //     WHITE,
         // );
@@ -388,21 +369,14 @@ async fn main() {
         draw_circle(coords.0, coords.1, 5.0, YELLOW);
         draw_text("Test beacon", coords.0, coords.1, 30.0, WHITE);
 
-        let coords = coord_to_screen_pos(-4800, -5200, &camera);
-        draw_circle(coords.0, coords.1, 15.0, BLUE);
-
-        // lod level matters
-        // camera zoom matters
-        // offset matters
-        // tile width matters
-
-        let sector_x = (camera.x_offset * camera.zoom_multiplier);
-        let tile_width = tile_dimensions.0 as f32 * camera.zoom_multiplier * two.powf(lod as f32);
-
         // draw blue line on map
         let coords1 = coord_to_screen_pos(-4800, -5200, &camera);
         let coords2 = coord_to_screen_pos(13000, 0, &camera);
         draw_line(coords1.0, coords1.1, coords2.0, coords2.1, 15.0, BLUE);
+
+        // draw dot at end of line
+        let coords = coord_to_screen_pos(-4800, -5200, &camera);
+        draw_circle(coords.0, coords.1, 15.0, BLUE);
 
         next_frame().await
     }
